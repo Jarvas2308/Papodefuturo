02317188155a518be52c0ee529976875b86ac043
Jarvas2308 <jarvas23@outlook.com>
2026-07-13 11:16:18 -0300

    feat: adiciona AuthProvider na aplicacao

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRouter } from './app/router/AppRouter'
+import { AuthProvider } from './auth/AuthProvider'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 )
