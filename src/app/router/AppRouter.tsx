7575651c0317aede1c5b8c2c2cc60040314b5dc0
Jarvas2308 <jarvas23@outlook.com>
2026-07-13 11:16:31 -0300

    feat: protege rotas internas com auth

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,4 +1,5 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { RequireAuth } from '../../auth/RequireAuth'
 import { AppShell } from '../../components/layout/AppShell'
 import { DashboardPage } from '../../pages/DashboardPage'
 import { HistoryPage } from '../../pages/HistoryPage'
@@ -14,13 +15,15 @@ export function AppRouter() {
     <Routes>
       <Route path="/" element={<Navigate to="/dashboard" replace />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route element={<AppShell />}>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/carteira" element={<PortfolioPage />} />
-        <Route path="/novo-aporte" element={<NewContributionPage />} />
-        <Route path="/historico" element={<HistoryPage />} />
-        <Route path="/estrategia" element={<StrategyPage />} />
-        <Route path="/configuracoes" element={<SettingsPage />} />
+      <Route element={<RequireAuth />}>
+        <Route element={<AppShell />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/carteira" element={<PortfolioPage />} />
+          <Route path="/novo-aporte" element={<NewContributionPage />} />
+          <Route path="/historico" element={<HistoryPage />} />
+          <Route path="/estrategia" element={<StrategyPage />} />
+          <Route path="/configuracoes" element={<SettingsPage />} />
+        </Route>
       </Route>
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
